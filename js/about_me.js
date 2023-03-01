2dd08e741fe51b8f4f43e9bcd9de9dc79c0b55a8
Alketa Memaj <memajalketa92@gmail.comgit config --global user.email memajalketa92@gmail.comgit config --global user.name Alketa>
2023-03-01 11:58:56 +0100

    positioning view my cv and making the script at about_me so that when the user hover it changes the style of the image and shows the button.

--- js/about_me.js
+++ js/about_me.js
@@ -1,1 +1,27 @@
-// JavaScript source code
+
+
+var view_my_cv_btn = document.getElementById("view_my_cv"); // get the view_my_cv button
+var cv_img = document.getElementById("cv_img"); // get the cv image on about_me
+
+cv_img.addEventListener("mouseover", (event) =>  /*add event to the cv image, so that when the user hovers on it
+                                                    the image opacity changes and the button is shown*/
+{
+    cv_img.style.opacity = "0.2";
+    view_my_cv_btn.style.display = "block";
+});
+
+/* add event to the cv image, so that when the user moves the mouse out
+ * it returns the element how they were*/
+cv_img.addEventListener("mouseout", (event) => {
+    cv_img.style.opacity = "1";
+    view_my_cv_btn.style.display = "none";
+});
+
+/*add the same event on mouse over also 
+ when the user hover the view my cv button*/
+
+view_my_cv_btn.addEventListener("mouseover", (event) => {
+    cv_img.style.opacity = "0.2";
+    view_my_cv_btn.style.display = "block";
+});
+
